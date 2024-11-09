43f4784f2fa0c2deca956141e4b63b2ea2a4ebfa
hiep1810 <33901647+hiep1810@users.noreply.github.com>
2024-11-09 23:46:34 +0700

    init again again again

--- src/components/FlashCard.js
+++ src/components/FlashCard.js
@@ -1,4 +1,38 @@
 import React, { useState } from 'react';
+import './FlashCard.css';
+
+const flashcards = [
+  {
+    category: "Present Simple",
+    question: "Công thức của thì hiện tại đơn?",
+    answer: "(+) S + V(s/es) + O\n(-) S + don't/doesn't + V + O\n(?) Do/Does + V + O?"
+  },
+  {
+    category: "Present Continuous",
+    question: "Công thức của thì hiện tại tiếp diễn?",
+    answer: "(+) S + am/is/are + Ving + O\n(-) S + am/is/are + not + Ving + O\n(?) Am/is/are + S + Ving + O?"
+  },
+  {
+    category: "Present Perfect",
+    question: "Công thức của thì hiện tại hoàn thành?",
+    answer: "(+) S + have/has + P2 + O\n(-) S + have/has + not + P2 + O\n(?) Have/has + S + P2 + O?"
+  },
+  {
+    category: "Past Simple",
+    question: "Công thức của thì quá khứ đơn?",
+    answer: "(+) S + V2/Ved + O\n(-) S + didn't + V + O\n(?) Did + S + V + O?"
+  },
+  {
+    category: "Past Continuous",
+    question: "Công thức của thì quá khứ tiếp diễn?",
+    answer: "(+) S + was/were + Ving + O\n(-) S + was/were + not + Ving + O\n(?) Was/were + S + Ving + O?"
+  },
+  {
+    category: "Future Simple",
+    question: "Công thức của thì tương lai đơn?",
+    answer: "(+) S + shall/will + V + O\n(-) S + shall/will + not + V + O\n(?) Shall/will + S + V + O?"
+  }
+]
 
 function FlashCard() {
   const [flipped, setFlipped] = useState(false);
@@ -8,17 +42,19 @@ function FlashCard() {
   };
 
   return (
-    <div className="flashcard" onClick={handleClick}>
-      {flipped ? (
-        <div className="back">
-          What is the capital of France?
-        </div>
-      ) : (
-        <div className="front">
-          Paris
-        </div>
-      )}
-    </div>
+    flashcards.map((flashcard) => (
+      <div className="flashcard" onClick={handleClick}>
+        {flipped ? (
+          <div className="back">
+            {flashcard.answer}
+          </div>
+        ) : (
+          <div className="front">
+            {flashcard.question}
+          </div>
+        )}
+      </div>
+    ))
   );
 }
 
